e04b2be6b29c266857adb437b3d05784369f1d63
irfanhfzh <irfanhafizh53@gmail.com>
2022-12-20 18:46:16 +0700

    add new challenges on frontend mentor

---
+++ frontend-mentor/challenges/newbie/article-preview-component-master/script.js
@@ -0,0 +1,33 @@
+function fadeIn(el, display) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function fadeOut(el) {
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= 0.1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+const handleClick = () => {
+  var x = document.getElementById("elementShare");
+  if (x.style.display === "none") {
+    fadeIn(x);
+  } else {
+    fadeOut(x);
+  }
+};
